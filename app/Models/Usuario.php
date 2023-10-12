b5b95f83fd7a15737e88093781dc068f146ac322
mama679 <ingmariomanotas@hotmail.com>
2023-10-12 13:34:39 -0500

    Se crearon los modelos usuario y ticket

---
+++ app/Models/Usuario.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Usuario extends Authenticatable
+{
+    use HasApiTokens,HasFactory, Notifiable;
+
+    protected $table = 'usuarios';
+    
+    protected $fillable = [
+        'usernom',
+        'nombres',
+        'email',
+        'password',
+    ];    
+
+    public function ticket(){
+        return $this->hasMany(Usuario::class);
+    }
+}
